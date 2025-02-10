fba5675a65b1210c5874c944463e4ec02735d365
Ademar Tellecher <ademartellecher@gmail.com>
2025-02-10 01:23:14 -0300

    Insatalado pacotes para uso do sqlite3, e feito alterações no arquivo service.go

--- core/beer/service.go
+++ core/beer/service.go
@@ -1,5 +1,10 @@
 package beer
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 // Define a interface com as funções que serão usadas pelo restante do projeto
 type UseCase interface {
 	GetAll() ([]*Beer, error)
@@ -13,30 +18,130 @@ type UseCase interface {
 // passa a ser uma implementação válida. Não existe a palavra "implements" como em Java ou PHP
 // desta forma, uma struct, uma string, um inteiro, etc, qualquer coisa pode ser válido, desde
 // que implemente todas as funções
-type Service struct{}
+type Service struct {
+	DB *sql.DB
+}
 
 // Esta função retorna um ponteiro em memoria para uma estrutura pre-definida
-func NewService() *Service {
-	return &Service{}
+func NewService(db *sql.DB) *Service {
+	return &Service{
+		DB: db,
+	}
 }
 
 // Declarando as (funções/metodos) que implementam a interface UseCase{}, que serão implentadas posteriomente
 func (s *Service) GetAll() ([]*Beer, error) {
-	return nil, nil
+	// Result é um slice de ponteiros do tipo Beer
+	// Se existir um erro, a função vai retorna-lo, e o erro deve ser tratado
+	// pelo chamador de função.
+	//Vamos sempre usar a conexão que esta dentro do Service
+
+	var result []*Beer
+
+	rows, err := s.DB.Query("select id, name, type, style from beers")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var b Beer
+
+		err = rows.Scan(&b.ID, &b.Name, &b.Type, &b.Style)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, &b)
+	}
+	return result, nil
 }
 
 func (s *Service) Get(ID int64) (*Beer, error) {
-	return nil, nil
+	// A variavel 'b' é do tipo Beer e recebe valores somente deste tipo
+	var b Beer
+
+	// Verificando se a consulta esta valida
+	stmt, err := s.DB.Prepare("select id, name, type, style from berrs where id=?")
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+	err = stmt.QueryRow(ID).Scan(&b.ID, &b.Name, &b.Type, &b.Style)
+	if err != nil {
+		return nil, err
+	}
+	// O retorno final da função, deve retornar a posição da memoria de da variavel'b'
+	return &b, nil
 }
 
 func (s *Service) Store(b *Beer) error {
+	// Iniciando uma transação com o banco de dados
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return err
+	}
+	stmt, err := tx.Prepare("insert into beers(id, name, type, style) values(?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	//o comando Exec, retorna um Result, mas não temos interesse nele, por isso podemos ignorá-lo com o '_' underline
+	_, err = stmt.Exec(b.ID, b.Name, b.Type, b.Style)
+	if err != nil {
+		tx.Rollback() // Se deu erro, desfaz todas as alterações ao estado inicial
+		return err
+	}
+	tx.Commit() // Commita no banco de dados, ou seja, grava no banco de dados
 	return nil
 }
 
 func (s *Service) Update(b *Beer) error {
+	if b.ID <= 0 {
+		return fmt.Errorf("ID %q invalido!..", b.ID)
+	}
+
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return err
+	}
+	stmt, err := tx.Prepare("update bbers set name=?, type=?, style=? where id=?")
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	//o comando Exec retorna um Result, mas não temos interesse nele, por isso podemos ignorá-lo com o _
+	_, err = stmt.Exec(b.ID, b.Name, b.Type, b.Style)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tx.Commit()
 	return nil
 }
 
 func (s *Service) Remove(ID int64) error {
+	if ID <= 0 {
+		return fmt.Errorf("ID: %q invalido!", ID)
+	}
+
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	//o comando Exec retorna um Result, mas não temos interesse nele, por isso podemos ignorá-lo com o _
+	_, err = tx.Exec("delete from beers where id=?", ID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tx.Commit()
 	return nil
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AdemarTellecher/pos-web-go
+
+go 1.23.6
+
+require github.com/mattn/go-sqlite3 v1.14.24 // indirect
